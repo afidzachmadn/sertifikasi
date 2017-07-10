943dd7ce470df8784daf5be7b76b92d7da0633c7
Muhammad Zuhri Hanifullah <zuhri2395@gmail.com>
2017-07-11 00:23:35 +0700

    disallow non authorized user to access dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class HomeController extends Controller
-{
-   public function dashboard()
-   {
-           return view('user.dashboard');
+class HomeController extends Controller {
+   public function dashboard() {
+           if($request->session()->get('login')) {
+            return view('user.dashboard');
+        } else {
+            return view('auth.login');
+        }
    }
 }
